2edc6927567e8d9d668f5c8de4b0c63430413413
Connor Kirkby <ikermitkiller@gmail.com>
2015-09-23 10:56:08 +0100

    Fixed JSlint problems

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
+/*global $, jQuery*/
 (function (window, $) {
-
+    "use strict";
     $(function () {
 
 
@@ -9,21 +10,21 @@
             var $div = $('<div/>'),
                 btnOffset = $(this).offset(),
                 xPos = event.pageX,
-                yPos = event.pageY;
+                yPos = event.pageY,
+                $ripple = $(".ripple-effect"),
+                toppos = yPos - ($(this).height() / 2);
 
 
 
             $div.addClass('ripple-effect');
-            var $ripple = $(".ripple-effect");
 
             $ripple.css("height", $(this).height() / 2);
             $ripple.css("width", $(this).height() / 2);
-            var toppos = yPos - ($(this).height() / 2)
             $div
                 .css({
                     'top': toppos - 40,
                     'left': xPos - 30,
-                    'background': $(this).data("ripple-colour"),
+                    'background': $(this).data("ripple-colour")
                 })
                 .appendTo($(this));
 
@@ -34,9 +35,10 @@
 
     });
 
-})(window, jQuery);
+}(window, jQuery));
 
 $(window).resize(function () {
+    "use strict";
     var viewportWidth = $(window).width();
     if (viewportWidth > 680) {
         document.getElementById('drawer').setAttribute('style', 'margin: 0;');
@@ -47,23 +49,27 @@ $(window).resize(function () {
 });
 
 function expandMore() {
+    "use strict";
     document.getElementById('text').setAttribute('class', 'expanded');
     document.getElementById('expand_more').setAttribute('style', 'display: none;');
     document.getElementById('expand_less').setAttribute('style', 'display: block;');
 }
 
 function expandLess() {
+    "use strict";
     document.getElementById('text').setAttribute('class', 'collapsed');
     document.getElementById('expand_more').setAttribute('style', 'display: block;');
     document.getElementById('expand_less').setAttribute('style', 'display: none;');
 }
 
 function showDrawer() {
+    "use strict";
     document.getElementById('drawer').setAttribute('style', 'margin: 0;');
     document.getElementById('drawer-off').setAttribute('style', 'display: inline-block');
 }
 
 function hideDrawer() {
+    "use strict";
     document.getElementById('drawer').setAttribute('style', 'margin-left: calc(-100% - 64px);');
     document.getElementById('drawer-off').setAttribute('style', 'display: none;');
 }
